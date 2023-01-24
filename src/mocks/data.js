3436c124d7858a5b41393751aec2d1eee3462c38
domuk-k <dannyworks102@gmail.com>
2023-01-25 03:55:51 +0900

    fix: add mock API - todo detail query

--- src/mocks/data.js
+++ src/mocks/data.js
@@ -1,114 +1,245 @@
-const todos = [
+export const todos = [
   {
-    id: 'fccd07cc-c617-482b-adc2-d03516ad6834',
-    title: 'shoulder',
+    id: 'b4dcf2df-3ff8-4441-b037-9adab856ea35',
+    title: 'tunnel',
+    done: false,
+  },
+  {
+    id: '4640b161-872d-4609-bc4d-f3a677466a48',
+    title: 'apply',
+    done: false,
+  },
+  {
+    id: 'b10f9116-2a28-4025-976d-dba1fa20a7e4',
+    title: 'authenticate',
+    done: false,
+  },
+  {
+    id: 'b84bb178-eb6e-4128-a9f6-79f4da563986',
+    title: 'grouch',
+    done: false,
+  },
+  {
+    id: 'b89383a2-edc5-429a-842c-7871d5ff963c',
+    title: 'mutilate',
+    done: false,
+  },
+  {
+    id: 'a0ecb490-0c41-4838-8934-f3b6f6532512',
+    title: 'experiment',
+    done: false,
+  },
+  {
+    id: '3acbdee7-c3cb-4b8b-a664-8008157e6ae0',
+    title: 'sort',
+    done: false,
+  },
+  {
+    id: '1c3bc0e5-f261-4b0c-8588-a557f4a8e76b',
+    title: 'domesticate',
+    done: false,
+  },
+  {
+    id: '68155e18-71dd-4fa2-9fd2-05104d359d63',
+    title: 'couch',
+    done: false,
+  },
+  {
+    id: '32090315-c62f-43f6-bc11-0ae366bdc6b7',
+    title: 'export',
+    done: false,
+  },
+  {
+    id: '74bd2cdf-3472-46de-82b1-e1b7e8c8ef2c',
+    title: 'ornament',
+    done: false,
+  },
+  {
+    id: '5ad2db84-3c46-4793-b293-f854570889d7',
+    title: 'blanket',
+    done: false,
+  },
+  {
+    id: 'b39af039-bbfe-400c-af49-92fd8203b3aa',
+    title: 'jest',
+    done: false,
+  },
+  {
+    id: '5f09a255-f404-4654-9f20-dd1cd29450c0',
+    title: 'emphasise',
+    done: false,
+  },
+  {
+    id: 'aa9da21b-3913-4aa9-86f4-462f3041a58e',
+    title: 'infringe',
+    done: false,
+  },
+  {
+    id: '89b7719d-54ec-4c53-ae2f-ae5ca5a40198',
+    title: 'solace',
     done: true,
   },
   {
-    id: 'fc6bb456-8d99-4b30-9f4d-f720045607b5',
-    title: 'crick',
+    id: '70ca0482-8ac0-4d39-8033-89316e255809',
+    title: 'inoculate',
     done: false,
   },
   {
-    id: '8983a567-5b83-40b2-aca2-d79da612e7e2',
-    title: 'federate',
-    done: false,
-  },
-  {
-    id: 'da9e1669-79a1-4409-80bc-50a18b895850',
-    title: 'liquidize',
-    done: false,
-  },
-  {
-    id: 'cab073ac-c65f-4861-af95-a1d9ba09b98d',
-    title: 'chide',
-    done: false,
-  },
-  {
-    id: '24bea108-c345-49ee-baea-405c01bb2589',
-    title: 'yellow',
-    done: false,
-  },
-  {
-    id: '424569cc-a928-451b-9130-ec604b5d8a68',
-    title: 'ululate',
-    done: false,
-  },
-  {
-    id: 'd3c8aaaa-694c-4ed6-9982-77b3704118d2',
-    title: 'scuttle',
-    done: false,
-  },
-  {
-    id: '996775cc-7df1-49c2-abbf-129349dd3493',
-    title: 'tattle',
-    done: false,
-  },
-  {
-    id: 'b6645c35-c371-42da-bfd4-2ca56104f7c1',
-    title: 'attain',
-    done: false,
-  },
-  {
-    id: 'a7a4e15a-dc65-4eef-b510-8c6b843b80c9',
-    title: 'impede',
-    done: false,
-  },
-  {
-    id: '87dae3e0-38a3-40c6-9443-71b4cb1522c1',
-    title: 'gum',
-    done: false,
-  },
-  {
-    id: 'fda38eaf-1b52-4d06-89ea-e71bea6079a6',
-    title: 'progress',
+    id: '59a98890-3bcd-433e-b92c-97b3a9eb6736',
+    title: 'bike',
     done: true,
   },
   {
-    id: 'f8874420-2761-4668-84f9-a42bc6d3d98c',
-    title: 'suction',
+    id: '3d2352e3-d118-44bf-81e4-10ceecb0a95c',
+    title: 'bill',
     done: false,
   },
   {
-    id: '74bc4f7c-fceb-4aad-b918-a4627cdf2b32',
-    title: 'commence',
+    id: '592c0c5e-60fd-495c-844f-d13e6cc560d7',
+    title: 'enthuse',
     done: false,
   },
   {
-    id: '8f42c7e3-80ec-42c5-8583-113fbc5d7e1f',
-    title: 'uncoil',
+    id: 'bf24ad1b-f7ff-4bf8-8103-c8c10021a754',
+    title: 'sympathize',
     done: false,
   },
   {
-    id: 'e2e1d472-fa6f-47e0-a323-668cbd926cfe',
-    title: 'pit',
-    done: true,
-  },
-  {
-    id: '4e194f51-a405-495f-b769-c37d48b47e0d',
-    title: 'reinstate',
+    id: '866feaa2-76d8-4b34-951f-0c40a400306d',
+    title: 'cauterize',
     done: false,
   },
   {
-    id: '858d88bb-4748-40d9-8f31-e0299f9d17f7',
-    title: 'reoccur',
+    id: 'a61f1b41-b893-4365-aa02-3f2de1cddf60',
+    title: 'ducks',
     done: false,
   },
   {
-    id: '335289d9-0e78-4875-81fc-e743b900ede7',
-    title: 'obey',
-    done: false,
-  },
-  {
-    id: 'b29ca81e-be6e-457d-8605-09814be283f7',
-    title: 'lunge',
-    done: false,
-  },
-  {
-    id: 'c867bcac-94c7-49bd-9cec-0f75bbdf551a',
-    title: 'hassle',
+    id: 'df936d38-14ce-4337-8f27-5837ceab9274',
+    title: 'invest',
     done: true,
   },
 ];
 
-export default todos;
+const mockDetails = [
+  {
+    id: 'b4dcf2df-3ff8-4441-b037-9adab856ea35',
+    description: 'We need to synthesize the back-end COM feed!',
+  },
+  {
+    id: '4640b161-872d-4609-bc4d-f3a677466a48',
+    description:
+      'Try to override the UDP microchip, maybe it will hack the mobile circuit!',
+  },
+  {
+    id: 'b10f9116-2a28-4025-976d-dba1fa20a7e4',
+    description: 'We need to transmit the open-source SMTP capacitor!',
+  },
+  {
+    id: 'b84bb178-eb6e-4128-a9f6-79f4da563986',
+    description:
+      'The UTF8 port is down, index the primary matrix so we can index the IB monitor!',
+  },
+  {
+    id: 'b89383a2-edc5-429a-842c-7871d5ff963c',
+    description:
+      "You can't reboot the firewall without calculating the redundant USB port!",
+  },
+  {
+    id: 'a0ecb490-0c41-4838-8934-f3b6f6532512',
+    description: 'We need to transmit the auxiliary COM bus!',
+  },
+  {
+    id: '3acbdee7-c3cb-4b8b-a664-8008157e6ae0',
+    description:
+      'Try to copy the SMS bandwidth, maybe it will connect the virtual hard drive!',
+  },
+  {
+    id: '1c3bc0e5-f261-4b0c-8588-a557f4a8e76b',
+    description:
+      'If we calculate the monitor, we can get to the XSS pixel through the bluetooth SQL application!',
+  },
+  {
+    id: '68155e18-71dd-4fa2-9fd2-05104d359d63',
+    description:
+      'If we compress the alarm, we can get to the COM bandwidth through the primary SAS feed!',
+  },
+  {
+    id: '32090315-c62f-43f6-bc11-0ae366bdc6b7',
+    description: 'We need to copy the virtual PNG port!',
+  },
+  {
+    id: '74bd2cdf-3472-46de-82b1-e1b7e8c8ef2c',
+    description:
+      'Use the wireless THX pixel, then you can quantify the neural monitor!',
+  },
+  {
+    id: '5ad2db84-3c46-4793-b293-f854570889d7',
+    description:
+      "I'll reboot the wireless HDD array, that should card the DRAM matrix!",
+  },
+  {
+    id: 'b39af039-bbfe-400c-af49-92fd8203b3aa',
+    description:
+      'Use the back-end CSS circuit, then you can connect the haptic alarm!',
+  },
+  {
+    id: '5f09a255-f404-4654-9f20-dd1cd29450c0',
+    description:
+      'Try to generate the AI card, maybe it will override the auxiliary monitor!',
+  },
+  {
+    id: 'aa9da21b-3913-4aa9-86f4-462f3041a58e',
+    description:
+      'Try to calculate the SMTP capacitor, maybe it will input the open-source sensor!',
+  },
+  {
+    id: '89b7719d-54ec-4c53-ae2f-ae5ca5a40198',
+    description:
+      "I'll synthesize the neural PCI driver, that should alarm the FTP bus!",
+  },
+  {
+    id: '70ca0482-8ac0-4d39-8033-89316e255809',
+    description:
+      'Use the multi-byte PNG pixel, then you can override the optical monitor!',
+  },
+  {
+    id: '59a98890-3bcd-433e-b92c-97b3a9eb6736',
+    description:
+      "You can't hack the bus without indexing the online JBOD sensor!",
+  },
+  {
+    id: '3d2352e3-d118-44bf-81e4-10ceecb0a95c',
+    description: 'We need to reboot the mobile RAM protocol!',
+  },
+  {
+    id: '592c0c5e-60fd-495c-844f-d13e6cc560d7',
+    description:
+      'If we transmit the alarm, we can get to the TLS feed through the mobile SAS sensor!',
+  },
+  {
+    id: 'bf24ad1b-f7ff-4bf8-8103-c8c10021a754',
+    description:
+      "I'll back up the back-end COM matrix, that should alarm the USB sensor!",
+  },
+  {
+    id: '866feaa2-76d8-4b34-951f-0c40a400306d',
+    description:
+      'If we bypass the alarm, we can get to the CSS panel through the 1080p RAM driver!',
+  },
+  {
+    id: 'a61f1b41-b893-4365-aa02-3f2de1cddf60',
+    description:
+      'Try to reboot the TCP monitor, maybe it will override the solid state port!',
+  },
+  {
+    id: 'df936d38-14ce-4337-8f27-5837ceab9274',
+    description:
+      "compressing the firewall won't do anything, we need to generate the online GB circuit!",
+  },
+];
+
+export const todoDetails = todos.map((todo, index) => ({
+  ...todo,
+  ...mockDetails.find((detail) => detail.id === todo.id),
+}));
--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,16 +1,26 @@
 import { rest } from 'msw';
 
-import todos from './data';
+import { todos, todoDetails } from './data';
 
 export const handlers = [
   // GET 할일 목록
   rest.get('/todos', async (req, res, ctx) => {
-    await sleep(200);
+    ctx.delay(200);
 
     return res(ctx.status(200), ctx.json(todos));
   }),
-];
 
-function sleep(ms) {
-  return new Promise((res) => setTimeout(res, ms));
-}
+  rest.get('/todo/:id', async (req, res, ctx) => {
+    ctx.delay(200);
+
+    let targetId = req.params.id;
+
+    if (!targetId) {
+      return res(ctx.status(400), ctx.json('id parameter is not found'));
+    }
+
+    const target = todoDetails.find((todo) => todo.id === targetId);
+
+    return res(ctx.status(200), ctx.json(target));
+  }),
+];
