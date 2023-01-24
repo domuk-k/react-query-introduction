78e74d997f041ebaaaf7a24c5d801b32469fadf3
domuk-k <dannyworks102@gmail.com>
2023-01-25 04:33:32 +0900

    fix: api delay implementation

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -5,14 +5,10 @@ import { todos, todoDetails } from './data';
 export const handlers = [
   // GET 할일 목록
   rest.get('/todos', async (req, res, ctx) => {
-    ctx.delay(200);
-
-    return res(ctx.status(200), ctx.json(todos));
+    return res(ctx.delay(700), ctx.status(200), ctx.json(todos));
   }),
 
   rest.get('/todo/:id', async (req, res, ctx) => {
-    ctx.delay(200);
-
     let targetId = req.params.id;
 
     if (!targetId) {
@@ -21,6 +17,6 @@ export const handlers = [
 
     const target = todoDetails.find((todo) => todo.id === targetId);
 
-    return res(ctx.status(200), ctx.json(target));
+    return res(ctx.delay(700), ctx.status(200), ctx.json(target));
   }),
 ];
