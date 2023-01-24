df90ef10e6c6eefc9d5258465fa03e577b99c6a9
domuk-k <dannyworks102@gmail.com>
2023-01-25 06:47:25 +0900

    fix: add UI - Pagination UI

---
+++ src/components/Pager.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+
+const PAGER_VIEW_PORT = 10;
+
+const getRange = (start = 0, end = 0) =>
+  Array.from({ length: end - start }, (_, index) => index + start);
+
+export default function Pager({ currentPage = 1, maxPage, onChange }) {
+  if (!maxPage) return null;
+
+  if (maxPage < currentPage) {
+    if (process.env.NODE_ENV !== 'production') {
+      throw new Error('currentPage exceeds maxPage');
+    }
+  }
+
+  const pageNumbers = (
+    Math.max(0, currentPage - PAGER_VIEW_PORT / 2)
+      ? Math.min(maxPage, currentPage + PAGER_VIEW_PORT / 2) === maxPage
+        ? // upper bounds
+          getRange(Math.max(0, maxPage - PAGER_VIEW_PORT), maxPage)
+        : // inner
+          getRange(
+            currentPage - PAGER_VIEW_PORT / 2,
+            currentPage + PAGER_VIEW_PORT / 2
+          )
+      : // bottom bounds
+        getRange(0, Math.min(maxPage, PAGER_VIEW_PORT))
+  ).map((index) => index + 1);
+
+  return (
+    <PagerContainer>
+      <button onClick={() => onChange(0)}>{'<'}</button>
+      {pageNumbers.map((number) => (
+        <PageNumberButton
+          isActive={number === currentPage}
+          onClick={() => onChange(number)}
+        >
+          {number}
+        </PageNumberButton>
+      ))}
+      <button onClick={() => onChange(maxPage)}>{'>'}</button>
+    </PagerContainer>
+  );
+}
+
+function PagerContainer({ children }) {
+  return (
+    <div
+      style={{
+        padding: '6px 25%',
+        fontSize: '12px',
+        display: 'flex',
+        justifyContent: 'space-between',
+      }}
+    >
+      {children}
+    </div>
+  );
+}
+
+function PageNumberButton({ isActive, children, onClick }) {
+  return (
+    <button
+      style={{
+        fontWeight: isActive ? 'bold' : 'lighter',
+        cursor: 'pointer',
+        borderRadius: '3px',
+        backgroundColor: isActive ? '#fff' : '#efefef',
+      }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
