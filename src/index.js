0347e2954f21048851d2c8cbb68c4e5468cc0f5a
domuk-k <dannyworks102@gmail.com>
2023-01-25 01:57:14 +0900

    feat: set query client provider to enable react-query hooks

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+
+const queryClient = new QueryClient();
+
+root.render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
+);
