69a82371a3f0839773a6b2a19382084d75f1abbc
domuk-k <dannyworks102@gmail.com>
2023-01-25 02:05:06 +0900

    fix: correct jsx intrinsic element prop names

--- src/components/EntryInput.jsx
+++ src/components/EntryInput.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function EntryInput() {
   return (
-    <div class="entry-layout">
-      <input class="entry-input" type="text" placeholder="할일 추가하기" />
-      <button class="entry-submit-button">
+    <div className="entry-layout">
+      <input className="entry-input" type="text" placeholder="할일 추가하기" />
+      <button className="entry-submit-button">
         <PlusIcon />
         <span>Add</span>
       </button>
@@ -15,15 +15,14 @@ export default function EntryInput() {
 function PlusIcon() {
   return (
     <svg
-      class="h-6 w-6"
       width="24"
       height="24"
       viewBox="0 0 24 24"
-      stroke-width="2"
+      strokeWidth="2"
       stroke="currentColor"
       fill="none"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     >
       <path stroke="none" d="M0 0h24v24H0z" /> <circle cx="12" cy="12" r="9" />{' '}
       <line x1="9" y1="12" x2="15" y2="12" />{' '}
