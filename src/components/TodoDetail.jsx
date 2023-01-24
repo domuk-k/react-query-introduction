e5af6e1d06995080947b2f4987bb42acb5344a35
domuk-k <dannyworks102@gmail.com>
2023-01-25 04:02:41 +0900

    docs: hints for applying useQuery

--- src/components/TodoDetail.jsx
+++ src/components/TodoDetail.jsx
@@ -7,7 +7,11 @@ const todo = {
   description: 'mock-description',
 };
 
+/**
+ * 이 컴포넌트는 외부로 부터 todo개체의 id를 받아서 해당 id로 todo상세정보를 조회하고, 표현합니다.
+ */
 function TodoDetail() {
+  // id를 props으로 받아오고, useQuery를 사용해서, `todo.description` 정보가 포함된 UI를 표현하세요.
   return (
     <>
       <section className="todo-detail-header">
