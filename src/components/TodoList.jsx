481e0d727c38027411b6acd75827f443659bdce4
domuk-k <dannyworks102@gmail.com>
2023-01-25 07:00:07 +0900

    fix: hint for exercise

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -4,20 +4,26 @@ import { TodoItem } from './TodoItem';
 import createQueryFn from '../util/createQueryFn';
 import { useQuery } from '@tanstack/react-query';
 import Loading from './Loading';
+import Pager from './Pager';
 
 const fetchTodos = createQueryFn('/todos');
 
 export default function TodoList() {
-  const { data, isLoading } = useQuery(['todos'], fetchTodos);
+  const { data, status } = useQuery(['todos'], fetchTodos);
 
   return (
     <ul className="todo-list-container">
-      {isLoading && <Loading />}
-      {data?.todos?.map((todo) => (
-        <TodoItem id={todo.id} done={todo.done} key={todo.id}>
-          {todo.title}
-        </TodoItem>
-      ))}
+      {status === 'loading' && <Loading />}
+      {status === 'success' && (
+        <>
+          {data?.todos?.map((todo) => (
+            <TodoItem id={todo.id} done={todo.done} key={todo.id}>
+              {todo.title}
+            </TodoItem>
+          ))}
+          <Pager maxPage={Math.ceil(data.totalCount / 10)} />
+        </>
+      )}
     </ul>
   );
 }
