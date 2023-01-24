a125af7b2832cc97f9e64101914d03a8e8c66cd5
domuk-k <dannyworks102@gmail.com>
2023-01-25 02:28:29 +0900

    fix: hints for applying useQuery

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { TodoItem } from './TodoItem';
 
+// import createQueryFn from '../util/fetcher'
+
 const todoData = [
   {
     id: 'b4dcf2df-3ff8-4441-b037-9adab856ea35',
@@ -14,7 +16,15 @@ const todoData = [
   },
 ];
 
+// useQuery에 전달할 쿼리함수를 생성하세요
+// const fetchTodos = createQueryFn('/todos');
+
 export default function TodoList() {
+  /**
+   * 여기서 useQuery를 사용하세요.
+   * 6번 줄의 todoData를 제거하고, useQuery로 가져온 데이터로 목록을 렌더링하세요.
+   */
+
   return (
     <ul className="todo-list-container">
       {todoData?.map((todo) => (
